8be686b29215aec8b466189a3379a0dc76142302
cyrexvibes <mosesbenjamin1985@gmail.com>
2026-03-10 12:15:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -126,8 +126,8 @@ async function sendAdminEmail(order) {
         Authorization: 'Bearer ${process.env.RESEND_API_KEY}',
       },
       body: JSON.stringify({
-        from: "Orders <onboarding@resend.dev">,
-        to: adminEmail,
+        from: "Orders-onboarding@resend.dev">,
+        to: process.env.ADMIN_Email,
         subject: `New order form submission: ${order.name || "Unknown"}`,
         text,
       }),
